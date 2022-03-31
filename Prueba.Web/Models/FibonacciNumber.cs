7751524127a7d12a34786cb6e846c768457a8bbf
Juan David Lagares Arrazola <juandavid@somosmakers.co>
2022-03-30 22:05:38 -0500

    Se agregan los modelos requeridos en la aplicacion

---
+++ Prueba.Web/Models/FibonacciNumber.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prueba.Web.Models
+{
+    public class FibonacciNumber
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El numero es requerido")]
+        [Display(Name = "Numero")] 
+        public int Number { get; set; }
+    }
+}
---
+++ Prueba.Web/Models/Operation.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prueba.Web.Models
+{
+    public class Operation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Valor A es requerido")]
+        [Display(Name = "Valor A")]
+        public int ValueA { get; set; }
+
+        [Required(ErrorMessage = "Valor B es requerido")]
+        [Display(Name = "Valor B")]
+        public int ValueB { get; set; }
+
+        [Required(ErrorMessage = "Resultado es requerido")]
+        [Display(Name = "Resultado")]
+        public int Result { get; set; }
+    }
+}
