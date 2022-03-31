f04fcd86f685d5706f86e5226a577ea183134fa1
Juan David Lagares Arrazola <juandavid@somosmakers.co>
2022-03-30 21:41:08 -0500

    Se modifica la vista del home y se agrega el formulario que se utilizara para la calculadora

--- Prueba.Web/ClientApp/src/components/Home.js
+++ Prueba.Web/ClientApp/src/components/Home.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
+import { Row, Col, FormGroup, Input, Label, Button } from 'reactstrap';
 
-export class Home extends Component {
-  static displayName = Home.name;
+export class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { valueA: 0, valueB: 0 };
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        alert('ValueA: ' + this.state.valueA + 'ValueB: ' + this.state.valueB);
+        this.setState({ result: parseInt(this.state.valueA) + parseInt(this.state.valueB) });
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Calculadora!</h1>
+                <p>Prueba tecnica Juan David Lagares Arrazola. Email: juandavidlagareas@gmail.com</p>
+                <form onSubmit={this.handleSubmit}>
+                    <Row>
+                        <Col xm="4">
+                            <FormGroup>
+                                <Label for="valueA">Valor A</Label>
+                                <Input type="number" name="valueA" id="valueA" placeholder="Valor A" value={this.state.valueA} onChange={this.handleChange}/>
+                            </FormGroup>
+                        </Col>
+                        <Col xs="4">
+                            <FormGroup>
+                                <Label for="valueB">Valor B</Label>
+                                <Input type="number" name="valueB" id="valueB" placeholder="Valor B" value={this.state.valueB} onChange={this.handleChange} />
+                            </FormGroup>
+                        </Col>
+                        <Col xs="4">
+                            <FormGroup>
+                                <Label for="valueB">Resultado</Label>
+                                <Input placeholder="Resultado" value={this.state.result} readOnly/>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <div class="mt-3">
+                        <Button color="primary">Calcular</Button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
