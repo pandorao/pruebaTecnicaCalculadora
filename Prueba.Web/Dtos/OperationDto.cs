e3ef73b11e5288eeb536dcbc0359d1de147f5184
Juan David Lagares Arrazola <juandavid@somosmakers.co>
2022-03-30 22:53:37 -0500

    Se agregan las dtos de la aplicacion

---
+++ Prueba.Web/Dtos/OperationDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prueba.Web.Dtos
+{
+    public class OperationDto
+    {
+        [Required(ErrorMessage = "Valor A es requerido")]
+        [Display(Name = "Valor A")]
+        public int ValueA { get; set; }
+
+        [Required(ErrorMessage = "Valor B es requerido")]
+        [Display(Name = "Valor B")]
+        public int ValueB { get; set; }
+    }
+}
---
+++ Prueba.Web/Models/ServiceResult.cs
@@ -0,0 +1,42 @@
+﻿namespace Prueba.Web.Models
+{
+    public class ServiceResult : AbstractServiceResult
+    {
+        public ServiceResult()
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+    }
+
+    public class ServiceResult<T> : AbstractServiceResult
+    {
+        public ServiceResult()
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        /// <summary>
+        /// Objeto de respuesta
+        /// </summary>
+        public T ResponseObject { get; set; }
+    }
+
+    public abstract class AbstractServiceResult
+    {
+        public StatusCode StatusCode { get; set; } = StatusCode.Error;
+
+        public Dictionary<string, string[]> Errors { get; set; }
+
+        public bool Succeeded()
+        {
+            return Errors.Count() == 0 && StatusCode == StatusCode.Succeded;
+        }
+    }
+
+    public enum StatusCode
+    {
+        Succeded = 0,
+        Warning = 1,
+        Error = 2
+    }
+}
