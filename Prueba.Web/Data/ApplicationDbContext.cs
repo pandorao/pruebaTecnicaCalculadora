c91318ef46d612547930fd2d368c4984ffdacbe8
Juan David Lagares Arrazola <juandavid@somosmakers.co>
2022-03-30 22:14:55 -0500

    Se agrega el dbcontext y se configura en el program.cs

---
+++ Prueba.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prueba.Web.Models;
+
+namespace Prueba.Web.Data
+{
+    public class ApplicationDbContext : DbContext       
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Operation> Operations { get; set; }
+        public DbSet<FibonacciNumber> FibonacciNumbers { get; set; }
+    }
+}
